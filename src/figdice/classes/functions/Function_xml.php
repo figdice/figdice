87e02a643604ff8b098c22638354f63830287912
Gabriel Zerbib <gabriel.zerbib@gmail.com>
2016-03-31 14:11:53 +0300

    DOMDocument default or explicit root node in xml func

--- src/figdice/classes/functions/Function_xml.php
+++ src/figdice/classes/functions/Function_xml.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @author Gabriel Zerbib <gabriel@figdice.org>
- * @copyright 2004-2014, Gabriel Zerbib.
- * @version 2.0.4
+ * @copyright 2004-2016, Gabriel Zerbib.
+ * @version 2.3.3
  * @package FigDice
  *
  * This file is part of FigDice.
@@ -25,8 +25,20 @@ namespace figdice\classes\functions;
 
 use \figdice\FigFunction;
 use \figdice\classes\ViewElementTag;
-use \figdice\LoggerFactory;
 
+/**
+ * Class Function_xml
+ *
+ * This class exposes the "xml( )" function to FigDice expressions.
+ * This function parses a string as an XML document, and returns a DOMXPath instance
+ * bound to this DOMDocument.
+ * If the XML parsing fails, a warning is triggered.
+ * The returned DOMXPath reference can be used in subsequent `evaluate` calls,
+ * through the "xpath( )" Fig function.
+ *
+ * A second string argument can be passed, to be used if the string cannot evaluate to valid XML due
+ * to the lack of a root node, in which case this 2nd arg is used as name of root tag.
+ */
 class Function_xml implements FigFunction {
 
 	/**
@@ -35,9 +47,15 @@ class Function_xml implements FigFunction {
 	 * @param array $arguments
 	 */
 	public function evaluate(ViewElementTag $viewElement, $arity, $arguments) {
-		$xmlString = '<xml>' . $arguments[0] . '</xml>';
+		$xmlString = $arguments[0];
 		$xml = new \DomDocument();
-		$xml->loadXML($xmlString, LIBXML_NOENT);
+		$successParse = @ $xml->loadXML($xmlString, LIBXML_NOENT);
+		if (! $successParse) {
+			$explicitRoot = $arity >= 2 ? $arguments[1] : 'xml';
+			$xmlString = '<'.$explicitRoot.'>' . $arguments[0] . '</'.$explicitRoot.'>';
+			// This time we let a warning be fired in case of invalid xml.
+			$xml->loadXML($xmlString, LIBXML_NOENT);
+		}
 		$xpath = new \DOMXPath($xml);
 		return $xpath;
 	}
---
+++ test/FunctionXmlTest.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * @author Gabriel Zerbib <gabriel@figdice.org>
+ * @copyright 2016, Gabriel Zerbib.
+ * @version 2.3.3
+ * @license GPLv3
+ * @package FigDice
+ *
+ * This file is part of FigDice.
+ */
+
+use figdice\View;
+
+class FunctionXmlTest extends PHPUnit_Framework_TestCase
+{
+  /**
+   * @expectedException PHPUnit_Framework_Error_Warning
+   */
+  public function testFunctionXmlForInvalidXmlIssuesWarning()
+  {
+    // Deliberately supplying invalid XML island,
+    // but because we're inside a Fig template which must be valid XML,
+    // we use CDATA to provide a string which will be considered as xml by DOM lib.
+    $xml = <<<TEMPLATE
+<fig:template>
+  <fig:mount target="myXml"><![CDATA[
+    <node1>value1
+    <node2>value2</node2>
+  ]]></fig:mount>
+  <fig:mute fig:text="xpath(xml(/myXml), '/xml/node1')"/>
+</fig:template>
+TEMPLATE;
+
+    $view = new View();
+    $view->loadString($xml);
+    $view->render();
+
+    $this->assertTrue(false, 'Should never occur: a Warning was caught before.');
+  }
+
+  public function testFunctionXmlWithoutRootNodeDefaultsToXmlRootNode()
+  {
+    $xml = <<<TEMPLATE
+<fig:template>
+  <fig:mount target="myXml">
+    <node1>value1</node1>
+    <node2>value2</node2>
+  </fig:mount>
+  <fig:mute fig:text="xpath(xml(/myXml), '/xml/node1')"/>
+</fig:template>
+TEMPLATE;
+
+    $view = new View();
+    $view->loadString($xml);
+    $output = trim($view->render());
+
+    $this->assertEquals('value1', $output);
+  }
+
+  public function testFunctionXmlWithoutRootNodeWithExplicitDefaultRoot()
+  {
+    $xml = <<<TEMPLATE
+<fig:template>
+  <fig:mount target="myXml">
+    <node1>value1</node1>
+    <node2>value2</node2>
+  </fig:mount>
+  <fig:mute fig:text="xpath(xml(/myXml, 'explicit'), '/explicit/node1')"/>
+</fig:template>
+TEMPLATE;
+
+    $view = new View();
+    $view->loadString($xml);
+    $output = trim($view->render());
+
+    $this->assertEquals('value1', $output);
+  }
+
+  public function testFunctionXmlWithRootUsesSpecified()
+  {
+    $xml = <<<TEMPLATE
+<fig:template>
+  <fig:mount target="myXml">
+    <myroot>
+      <node1>value1</node1>
+      <node2>value2</node2>
+    </myroot>
+  </fig:mount>
+  <fig:mute fig:text="xpath(xml(/myXml), '/myroot/node1')"/>
+</fig:template>
+TEMPLATE;
+
+    $view = new View();
+    $view->loadString($xml);
+    $output = trim($view->render());
+
+    $this->assertEquals('value1', $output);
+  }
+}
