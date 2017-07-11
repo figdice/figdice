a9ed2a60b7e58ec259bc4ff72df2f67ad3203719
Gabriel Zerbib <gabriel.zerbib@gmail.com>
2017-07-11 13:55:25 +0300

    Changed license to MIT

--- src/figdice/classes/XMLEntityTransformer.php
+++ src/figdice/classes/XMLEntityTransformer.php
@@ -7,18 +7,8 @@
  *
  * This file is part of FigDice.
  *
- * FigDice is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
  *
- * FigDice is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with FigDice.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace figdice\classes;
